c7ce00656a694c05e8f15721d1bc22addf539310
J. Garcia <janikgar@pm.me>
2026-03-15 00:09:06 -0700

    port shell.nix to flake.nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "My shell.nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/?ref=25.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in
+      pkgs.mkShell {
+      packages = with pkgs; [
+        vim
+        git
+        go
+        python314
+        nodejs_24
+        yarn-berry
+        kubectl
+        localstack
+        go-jsonnet
+        zsh
+        oh-my-zsh
+        go-task
+        nix-info
+      ];
+    };
+  };
+}
